04c601210211eb2ca9fc4a543a61e86521d459f7
EstevesHERE <leonardo-ec@hotmail.com>
2024-10-15 20:35:13 -0300

    feat: add unit test and workflow

--- src/main/java/br/com/app/coleta/lixo/dto/ColetaDTO.java
+++ src/main/java/br/com/app/coleta/lixo/dto/ColetaDTO.java
@@ -1,18 +1,19 @@
 package br.com.app.coleta.lixo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ColetaDTO {
 
     @JsonProperty("id_coleta")
-    public long idColeta ;
+    public long idColeta;
     @JsonProperty("nome_bairro")
     public String nomeBairro;
     public Date dataColeta;
