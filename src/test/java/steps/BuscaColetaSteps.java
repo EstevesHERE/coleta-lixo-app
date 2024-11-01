4f2cb2f9fb468078f5ee00bed0217eb752b68b68
Vanessa Bertoldo <126096822+Vanessa-Bertoldo@users.noreply.github.com>
2024-10-31 23:05:35 -0300

    Correcao so JSON Shema

--- src/test/java/steps/BuscaColetaSteps.java
+++ src/test/java/steps/BuscaColetaSteps.java
@@ -33,25 +33,18 @@ public class BuscaColetaSteps {
 
     @Então("o status code da resposta da busca deve ser {int}")
     public void oStatusCodeDaRespostaDaBuscaDeveSer(int statusCode) {
-        response.then().statusCode(statusCode)
-                .and()
-                .body("idColeta", equalTo(2))
-                .body("nomeBairro", equalTo("Vila cesar"))
-                .body("numeroVolume", equalTo(15000.0F))
-                .body("dataColeta", equalTo("2024-05-03T03:00:00.000+00:00"))
-                .body("dataRegistro", equalTo("2024-05-26T21:40:43.000+00:00"))
-                .body("rota", equalTo(null));
-
         try {
             JsonSchema schema = buscaColetaService.loadJsonSchema("src/test/resources/schemas/busca-bem-sucedido-coleta.json");
             JSONObject jsonResponse = new JSONObject(response.asString());
             Set<ValidationMessage> validationMessages = buscaColetaService.validateJsonSchema(schema, jsonResponse);
 
             if (!validationMessages.isEmpty()) {
-                throw new AssertionError("JSON does not match schema: " + validationMessages);
+                throw new AssertionError("O contrato está invélido. Erros do contrato: " + validationMessages);
             }
+            response.then().statusCode(statusCode);
+
         } catch (Exception e) {
-            throw new AssertionError("Error loading or validating JSON schema: " + e.getMessage());
+            throw new AssertionError("Erro ao validar contrato: " + e.getMessage());
         }
     }
 }
